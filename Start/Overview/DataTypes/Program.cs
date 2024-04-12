fe88524b808b3407d4a6fa874d50fafcf843c059
Nathan Peters <peters.nate8@gmail.com>
2024-04-12 15:17:03 -0600

    1.3 basic .NET data types

--- Start/Overview/DataTypes/Program.cs
+++ Start/Overview/DataTypes/Program.cs
@@ -12,9 +12,17 @@ bool tf = default;
 Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
 
 // TODO: implicit type conversion
+Console.WriteLine($"{c + a}"); // 66
+Console.WriteLine($"{(char)(c + a)}"); // B
+
+Console.WriteLine($"{f + a}"); // 124.45
+Console.WriteLine($"{f + c}"); //188.45
 
 
 // TODO: Create an instance of a struct (which is a value type)
+s s1;
+s1.a = 5;
+s1.b = false;
 
 
 // Perform an operation on a struct
@@ -25,13 +33,15 @@ void StructOp(s theStruct) {
     Console.WriteLine($"{theStruct.a}, {theStruct.b}");
 }
 
-// Console.WriteLine("Structs are passed by copy, since they are value types:");
-// Console.WriteLine($"{s1.a}, {s1.b}");
-// StructOp(s1);
-// Console.WriteLine($"{s1.a}, {s1.b}");
+//` using a struct will not change the s1 variables
+Console.WriteLine("Structs are passed by copy, since they are value types:");
+Console.WriteLine($"{s1.a}, {s1.b}"); //5, False
+StructOp(s1); //10, True
+Console.WriteLine($"{s1.a}, {s1.b}"); //5, False
 
 // TODO: Create an object instance of a class (which is a reference type)
 
+MyClass cl = new MyClass{a = 5, b = false};
 
 // Perform an operation on the class
 void ClassOp(MyClass theClass) {
@@ -41,10 +51,11 @@ void ClassOp(MyClass theClass) {
     Console.WriteLine($"{theClass.a}, {theClass.b}");
 }
 
-// Console.WriteLine("Objects are passed by reference, since they are reference types:");
-// Console.WriteLine($"{cl.a}, {cl.b}");
-// ClassOp(cl);
-// Console.WriteLine($"{cl.a}, {cl.b}");
+//` using a class will change the c1 class variables
+Console.WriteLine("Objects are passed by reference, since they are reference types:");
+Console.WriteLine($"{cl.a}, {cl.b}"); // 5, False
+ClassOp(cl); // 10, True
+Console.WriteLine($"{cl.a}, {cl.b}"); //10, True
 
 // These are declared at the bottom of the file because C# requires
 // top-level statements to come before type declarations
